b7477dcb6dc43b829e79ceaf2195e787ee42a648
Benoit Ruiz <benoit.ruiz01@gmail.com>
2023-07-15 11:21:16 +0200

    Add unit tests for day 2

---
+++ src/day2/part2/index.spec.ts
@@ -0,0 +1,18 @@
+import { describe, expect, it } from 'vitest'
+import { program } from './index.js'
+import { Effect } from 'effect'
+import { InputProvider } from '../../common/index.js'
+
+describe('Day 2 part 2', () => {
+  it('should solve example puzzle', () => {
+    const runnable = Effect.provideService(
+      program,
+      InputProvider,
+      InputProvider.of({
+        get: () => Effect.succeed(['A Y', 'B X', 'C Z']),
+      })
+    )
+
+    expect(Effect.runSync(runnable)).toEqual(12)
+  })
+})
