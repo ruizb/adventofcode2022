8875864113d42cbb86f3608e6115ed64cca09702
Benoit Ruiz <benoit.ruiz01@gmail.com>
2023-07-16 20:01:12 +0200

    Update InputProvider to fail with FileError instead of unknown

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,15 +1,29 @@
 import { promises as fs } from 'node:fs'
 import { Context, Effect, List } from 'effect'
 
+class FileError extends Error {
+  readonly _tag = 'FileError'
+}
+
 export interface InputProvider {
-  readonly get: (filepath: URL) => Effect.Effect<never, unknown, string[]>
+  readonly get: (filepath: URL) => Effect.Effect<never, FileError, string[]>
 }
 
 export const InputProvider = Context.Tag<InputProvider>()
 
+const readFile = (filepath: URL): Effect.Effect<never, FileError, string> =>
+  Effect.tryPromise(() => fs.readFile(filepath, 'utf-8')).pipe(
+    Effect.mapError(
+      cause =>
+        new FileError(`Error when reading file: ${filepath.toJSON()}`, {
+          cause,
+        })
+    )
+  )
+
 export const InputProviderLive = InputProvider.of({
   get: (filepath: URL) => {
-    return Effect.tryPromise(() => fs.readFile(filepath, 'utf-8')).pipe(
+    return readFile(filepath).pipe(
       Effect.map(fileContents => fileContents.trimEnd().split('\n'))
     )
   },
