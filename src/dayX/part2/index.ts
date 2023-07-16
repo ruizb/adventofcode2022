d12642974f5520df6b51b561ee8337724e5e9ad1
Benoit Ruiz <benoit.ruiz01@gmail.com>
2023-07-16 20:38:21 +0200

    Fix filepath for dayX/part2

--- src/dayX/part2/index.ts
+++ src/dayX/part2/index.ts
@@ -3,7 +3,7 @@ import { InputProvider } from '../../common/index.js'
 
 export const program = InputProvider.pipe(
   Effect.flatMap(inputProvider =>
-    inputProvider.get(new URL('input.txt', import.meta.url))
+    inputProvider.get(new URL('../part1/input.txt', import.meta.url))
   ),
   Effect.map(() => 42)
 )
